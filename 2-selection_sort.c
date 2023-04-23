97846a106773b05a075e56c12196237d58f5ef66
AdrianWolfP <wolfy52032@gmail.com>
2023-04-23 05:59:59 -0700

    fixed file 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,39 +1,34 @@
 #include "sort.h"
 /**
- * swap_array - function that swaps two array elements
- * @array: Array
- * @s: Size of array
- * @a: index of first element
- * @b: index of second element
- */
-
-void swap_array(int *array, size_t s, int a, int b)
-{
-	int t = array[a];
-
-	array[a] = array[b];
-	array[b] = t;
-	print_array(array, s);
-}
-
-/**
  * selection_sort - Selction sort sorts array
  * @array: Array
  * @size: Size of array
+ * Return: Void
  */
 
 void selection_sort(int *array, size_t size)
 {
-	int min, index, counterIndex;
+	size_t i, j, min;
+	int tmp;
 
-	for (counterIndex = 0; counterIndex < (int)size - 1; counterIndex++)
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (index = min = counterIndex; index < (int)size; index++)
+		min = 1;
+
+		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] > array[index])
-				min = index;
+			if (array[min] > array[j])
+				min = j;
 		}
-		if (counterIndex != min)
-			swap_array(array, size, counterIndex, min);
+		if (min != i)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
 	}
 }
