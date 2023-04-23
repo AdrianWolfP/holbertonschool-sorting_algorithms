9808f99fd2408e7bde1de6e01888e447a5e781d3
AdrianWolfP <wolfy52032@gmail.com>
2023-04-23 05:52:19 -0700

    fixed file 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,61 +1,40 @@
 #include "sort.h"
 /**
- * swap_list - swap tow nodes
- * @nodeA: Fisrt node
- * @nodeB: Second node
- */
-
-void swap_list(listint_t *nodeA, listint_t *nodeB)
-{
-	listint_t *placeholder;
-
-	placeholder = nodeA->next;
-	nodeA->next = nodeB->next;
-	nodeB->next = placeholder;
-
-	if (nodeA->next)
-		nodeA->next->prev = nodeA;
-	if (nodeB->next)
-		nodeB->next->prev = nodeB;
-	placeholder = nodeA->prev;
-	nodeA->prev = nodeB->prev;
-	nodeB->prev = placeholder;
-
-	if (nodeA->prev)
-		nodeA->prev->next = nodeA;
-	if (nodeB->prev)
-		nodeB->prev->next = nodeB;
-	while (placeholder->prev)
-		placeholder = placeholder->prev;
-	print_list(placeholder);
-}
-
-/**
  * insertion_sort_list - Insertion_sort sorts a list
- * @list List that is sorted
+ * @list: List that is sorted
+ * Return: void
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *index, *head, *sub_index;
+	listint_t *current, *next, *prev;
 
-	if (!(list) || !(list))
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-	head = *list;
-	while (head->prev)
-		head = head->prev;
-	for (index = head; index;
-	     index = (sub_index ? sub_index->next : index->next))
-	{ /* loop from head to tail */
-		for (sub_index = index; sub_index = sub_index = sub_index->prev)
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		next = current->next;
+		prev = current->prev;
+
+		while (prev != NULL && prev->n > current->n)
 		{
-			if (sub_index->prev && sub_index->n < sub_index->prev->n)
-				swap_list(sub_index, sub_index->prev);
+			prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
+			prev->prev = current;
+
+			if (current->prev == NULL)
+			*list = current;
+
 			else
-				break;
+				current->prev->next = current;
+			print_list(*list);
+			prev = current->prev;
 		}
+		current = next;
 	}
-	while (head->prev)
-		head = head->prev;
-	*list - head;
 }
