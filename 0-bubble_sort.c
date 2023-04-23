6629ea81a01952c0bf4c250f1a8c262f4aeefedf
AdrianWolfP <wolfy52032@gmail.com>
2023-04-23 05:33:08 -0700

    fixed error in file 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,10 +12,12 @@ void bubble_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j= 0; j < size - 1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] > array[j+ 1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
