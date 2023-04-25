6dbd59f1b10b0d3dae361f8704214807b5ff7039
AdrianWolfP <wolfy52032@gmail.com>
2023-04-24 21:24:51 -0700

    g

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,27 +8,40 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int tmp;
+	size_t i = 0, j;
+	int temp = 0;
+	int temp_i = 0;
+	int flag = 0;
 
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
+	while (i < (size - 1))
 	{
-		min = 1;
-
+		flag = 0;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] > array[j])
-				min = j;
+			if (flag == 0)
+			{
+				if (array[i] > array[j])
+				{
+					flag = 1;
+					temp = array[j];
+					temp_i = j;
+				}
+			}
+			else
+			{
+				if (temp > array[j])
+				{
+					temp = array[j];
+					temp_i = j;
+				}
+			}
 		}
-		if (min != i)
+		if (flag == i)
 		{
-			tmp = array[i];
-			array[i] = array[min];
-			array[min] = tmp;
+			array[temp_i] = array[i];
+			array[i] = temp;
 			print_array(array, size);
 		}
+		i++;
 	}
 }
